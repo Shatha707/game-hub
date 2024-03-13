f80c76bbf72a2028283623cd2e045e95973397d8
shatha <shathaa707@gmail.com>
2024-03-14 00:42:04 +0300

    Filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,4 +1,4 @@
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { Game } from "./useGames";
@@ -9,7 +9,7 @@ import { Game } from "./useGames";
     results: T[];
  }
 
-const useData = <T>(endpoint:string) => {
+const useData = <T>(endpoint:string, requestConfig? : AxiosRequestConfig , deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
         const [error, setError] = useState("");
         const [isLoading, setLoading]= useState(false);
@@ -19,7 +19,7 @@ const useData = <T>(endpoint:string) => {
           
                 setLoading(true);
             apiClient
-            .get<FetchsResponse<T>>(endpoint, {signal: controller.signal})
+            .get<FetchsResponse<T>>(endpoint, {signal: controller.signal,...requestConfig})
             .then((res) => {
                 
                 setData(res.data.results);
@@ -32,7 +32,7 @@ const useData = <T>(endpoint:string) => {
             });
 
             return () => controller.abort();
-        },[]);
+        },deps ?[...deps]:[]);
         return{data,error, isLoading}
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 
 
@@ -18,5 +19,5 @@ import useData from "./useData";
  
 
 
- const useGames=()=> useData <Game> ('/games');
+ const useGames=(selectedGenre:Genre| null)=> useData <Game> ('/games',{params: {genres: selectedGenre?.id}}, [selectedGenre?.id]);
  export default useGames
