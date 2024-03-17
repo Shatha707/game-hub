db3a4eb6df577b1ed43fd3d2102a638b3ae05423
shatha <shathaa707@gmail.com>
2024-03-17 15:31:31 +0300

    build platform selector 2 - after check-

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
-import { BsChevronBarDown } from "react-icons/bs";
+import { BsChevronBarDown, BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/useGames";
 
@@ -10,7 +10,7 @@ const PlatformSelector = () => {
   if (error) return null;
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         Platforms
       </MenuButton>
       <MenuList>
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -5,5 +5,5 @@ interface Platform{
     name: string;
     slug: string;
 }
-const usePlatforms=() => useData<Platform>('/platforms/lists/parents')
+const usePlatforms =() => useData<Platform>('/platforms/lists/parents')
 export default usePlatforms;
