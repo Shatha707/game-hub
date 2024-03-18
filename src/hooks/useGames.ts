1ba2da3bf66baf598d35e39d3a8a718bf8ad6077
shatha <shathaa707@gmail.com>
2024-03-19 01:32:56 +0300

    add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,8 @@ export interface Game {
     background_image: string;
     parent_platforms: { platform: Platform }[];
     metacritic: number;
+    rating_top: number;// normal
+   // rating: number; // float number
 }
 
 const useGames = (gameQuery: GameQuery) =>
